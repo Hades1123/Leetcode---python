7d96c2998d1481d314016c21b542847c7f05a407
Hades1123 <dp1.1a7.si@gmail.com>
2025-08-06 10:17:51 +0700

    prefix sum

--- prefix sum/974_Subarray Sums Divisible by K.py
+++ prefix sum/974_Subarray Sums Divisible by K.py
@@ -3,19 +3,14 @@ from typing import List
 
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        count = 0
-        prefix_sum = 0
-        prefix_map = {0: 1}  
-        
+        current_sum = result = 0
+        dict = {0 : 1}
         for num in nums:
-            prefix_sum += num
-            mod = prefix_sum % k
-            if mod < 0:  
-                mod += k
-            if mod in prefix_map:
-                count += prefix_map[mod]
-                prefix_map[mod] += 1
+            current_sum += num
+            if current_sum % k in dict:
+                result += dict[current_sum % k]
+                dict[current_sum % k] += 1
             else:
-                prefix_map[mod] = 1
-        
-        return count
+                dict[current_sum % k] = 1
+        return result
+            
