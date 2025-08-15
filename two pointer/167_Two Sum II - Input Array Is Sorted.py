74e6f53f95f7558b7d30918fc8be618acb5d6680
Hades1123 <dp1.1a7.si@gmail.com>
2025-08-15 10:23:28 +0700

    two pointer

--- two pointer/167_Two Sum II - Input Array Is Sorted.py
+++ two pointer/167_Two Sum II - Input Array Is Sorted.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
+        start = 0
+        end = len(numbers) - 1
+        while start < end:
+            currentSum = numbers[start] + numbers[end]
+            if  currentSum == target:
+                return [start + 1, end  + 1]
+            elif currentSum < target:
+                start += 1
+            else:
+                end -= 1
+
+        return [1, 2]
