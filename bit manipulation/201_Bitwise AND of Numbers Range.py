b25facc10c57bf2b1f68a630824f8b886c996f78
Hades1123 <dp1.1a7.si@gmail.com>
2025-08-18 23:10:38 +0700

    manipulation

--- bit manipulation/201_Bitwise AND of Numbers Range.py
+++ bit manipulation/201_Bitwise AND of Numbers Range.py
@@ -1,24 +1,8 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        count  = 0
-        num = 0
-        temp = right
-        while temp:
-            num += 1
-            if ~ ((left & 1) ^ (right & 1)):
-                count += 1
-            else:
-                count += 1
-                num += count
-                temp = temp >> count
-                count = 0
-            right = right >> count
-            left = left >> count
-
-        if count:
-            right = right >> count
-        while num:
-            temp = temp << 1
-            num -= 1
-        return temp
-            
+        count = 0
+        while left != right:
+            left >>= 1
+            right >>= 1
+            count += 1
+        return left << count
