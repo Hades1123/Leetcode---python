2f6d482991547a633a0a81074998f98b7cfeb8c9
Hades1123 <dp1.1a7.si@gmail.com>
2025-08-24 09:06:58 +0700

    3-medium-hashset + array

--- sliding window/3_Longest Substring Without Repeating Characters.java
+++ sliding window/3_Longest Substring Without Repeating Characters.java
@@ -1,37 +1,15 @@
-import java.util.HashSet;
-import java.util.Set;
-
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         int begin = 0, end = 0, len = 0;
-//         Map<Character, Integer> map = new HashMap<>();
-//         while (end < s.length()) {
-//             char current = s.charAt(end);
-//             map.put(current, map.getOrDefault(current, 0) + 1);
-//             while (map.get(current) > 1) {
-//                 char temp = s.charAt(begin);
-//                 map.put(temp, map.get(temp) - 1);
-//                 begin++;
-//             }
-//             end++;
-//             len = Math.max(len, end - begin);
-//         }
-//         return len;
-//     }
-// }
-
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int begin = 0, end = 0, maxLen = 0;
-        Set<Character> set = new HashSet<>();
-        for (end = 0; end < s.length(); ++end) {
-            char current = s.charAt(end);
-            while (set.contains(current)) {
-                set.remove(s.charAt(begin++));
+        int[] arr = new int[128];
+        int result = 0, start = 0;
+        for (int end = 0; end < s.length(); ++end) {
+            arr[s.charAt(end)] += 1;
+            while (arr[s.charAt(end)] > 1) {
+                arr[s.charAt(start)] -= 1;
+                start += 1;
             }
-            set.add(current);
-            maxLen = Math.max(maxLen, end - begin + 1);
+            result = Math.max(result, end - start + 1);
         }
-        return maxLen;
+        return result;
     }
 }
