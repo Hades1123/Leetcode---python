955d5cf476fb00e9b15b1e96a976d9d517602aa5
Hades1123 <dp1.1a7.si@gmail.com>
2025-08-24 09:25:57 +0700

    3-medium-array + hashet

--- sliding window/3_Longest Substring Without Repeating Characters.java
+++ sliding window/3_Longest Substring Without Repeating Characters.java
@@ -1,13 +1,32 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int[] arr = new int[128];
+//         int result = 0, start = 0;
+//         for (int end = 0; end < s.length(); ++end) {
+//             arr[s.charAt(end)] += 1;
+//             while (arr[s.charAt(end)] > 1) {
+//                 arr[s.charAt(start)] -= 1;
+//                 start += 1;
+//             }
+//             result = Math.max(result, end - start + 1);
+//         }
+//         return result;
+//     }
+// }
+
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int[] arr = new int[128];
+        Set<Character> set = new HashSet<>();
         int result = 0, start = 0;
         for (int end = 0; end < s.length(); ++end) {
-            arr[s.charAt(end)] += 1;
-            while (arr[s.charAt(end)] > 1) {
-                arr[s.charAt(start)] -= 1;
-                start += 1;
+            char current = s.charAt(end);
+            while (set.contains(current)) {
+                set.remove(s.charAt(start++));
             }
+            set.add(current);
             result = Math.max(result, end - start + 1);
         }
         return result;
